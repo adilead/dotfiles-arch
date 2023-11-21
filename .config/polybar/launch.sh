8bf088c9989b072945a7e26fb9f23b5541c49844
adilead <bleier.adrian@gmx.de>
2023-11-21 23:27:54 +0100

    changed everything to gruvbox

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,6 +10,7 @@ killall -q polybar
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 # polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-polybar
+polybar mainscreen &
+polybar secondscreen &
 
 echo "Bars launched..."
